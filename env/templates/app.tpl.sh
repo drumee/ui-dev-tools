53452e54609f5ebb87e94d0e6412c6475e8a18c4
Somanos Sar <somanos@drumee.net>
2026-04-23 23:48:24 +0700

    Changed templates

--- env/templates/app.tpl.sh
+++ env/templates/app.tpl.sh
@@ -1,8 +1,8 @@
-export ENDPOINT=<%= user %>
+export ENDPOINT=<%= endpoint %>
 export REMOTE_USER=<%= user %>
 export UI_RUNTIME_HOST=<%= server %>
 export BUILD_TARGET=<%= type %>
-export UI_RUNTIME_PATH=/srv/drumee/runtime/ui/$ENDPOINT
+export UI_RUNTIME_PATH=<%= runtime_dir %>/ui/$ENDPOINT
 export UI_BUILD_MODE=development
 export PUBLIC_PATH=/-/$ENDPOINT/$BUILD_TARGET/
 
