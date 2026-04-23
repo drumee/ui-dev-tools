3c574b8cfcc7f0dab066c76984a9530086f09593
Somanos Sar <somanos@drumee.net>
2026-04-23 23:52:31 +0700

    Fixed templates

--- env/templates/plugin.tpl.sh
+++ env/templates/plugin.tpl.sh
@@ -5,4 +5,4 @@ export UI_BUILD_PATH=$HOME/build/<%= endpoint %>/plugins/<%= plugin %>
 export UI_BUILD_MODE=development
 export OUTPUT_FILENAME="[name]-[fullhash].js"
 export PUBLIC_PATH=/-/<%= endpoint %>/plugins/<%= plugin %>/ 
-export BUILD_TARGET=<%= plugin %>/
+export BUILD_TARGET=<%= plugin %>
