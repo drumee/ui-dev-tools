1f77f0035411ff65ae63c4933829c362684ee817
Somanos Sar <somanos@drumee.com>
2026-06-12 18:11:59 +0700

    Normalized path

--- env/templates/plugin.tpl.sh
+++ env/templates/plugin.tpl.sh
@@ -1,12 +1,12 @@
 export REMOTE_USER=<%= user %>
+export BUILD_TARGET=<%= plugin %>
 export UI_RUNTIME_HOST=<%= server %>
-export UI_RUNTIME_PATH=<%= runtime_dir %>/plugins/ui/<%= endpoint %>/<%= plugin %>
-export UI_BUILD_PATH=$HOME/build/<%= endpoint %>/plugins/<%= plugin %>
+export UI_RUNTIME_PATH=<%= runtime_dir %>/plugins/ui/<%= endpoint %>/$BUILD_TARGET
+export UI_BUILD_PATH=$HOME/build/<%= endpoint %>/plugins/$BUILD_TARGET
 export UI_BUILD_MODE=<%= build_mode %>
 export OUTPUT_FILENAME="[name]-[fullhash].js"
-export BUILD_TARGET=<%= plugin %>
 <% if (endpoint==="main") { %>
-export PUBLIC_PATH=/-/plugins/<%= plugin %>/ 
+export PUBLIC_PATH=/-/plugins/$BUILD_TARGET/ 
 <% } else { %> 
-export PUBLIC_PATH=/-/<%= endpoint %>/plugins/<%= plugin %>/ 
+export PUBLIC_PATH=/-/<%= endpoint %>/plugins/$BUILD_TARGET 
 <% } %>
