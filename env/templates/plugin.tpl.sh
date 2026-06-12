4ec4978c74ace8cbd2c11c8e0d437dc715ebbb0e
Somanos Sar <somanos@drumee.com>
2026-06-12 17:29:46 +0700

    Fixed templates/plugin

--- env/templates/plugin.tpl.sh
+++ env/templates/plugin.tpl.sh
@@ -1,6 +1,6 @@
 export REMOTE_USER=<%= user %>
 export UI_RUNTIME_HOST=<%= server %>
-export UI_RUNTIME_PATH=<%= runtime_dir %>/plugins/ui/<%= endpoint %>/
+export UI_RUNTIME_PATH=<%= runtime_dir %>/plugins/ui/<%= endpoint %>/<%= plugin %>
 export UI_BUILD_PATH=$HOME/build/<%= endpoint %>/plugins/<%= plugin %>
 export UI_BUILD_MODE=<%= build_mode %>
 export OUTPUT_FILENAME="[name]-[fullhash].js"
